f1aa1af4e753880bb1aa19b33ced2eca27539ef2
Zulfa Hidayat <zulfahidayat13@gmail.com>
2022-09-23 16:38:27 +0700

    commit pertama

---
+++ 50bilanganGanjil.js
@@ -0,0 +1,8 @@
+let awal = 1
+
+for (let i = awal; i < 100; i++){
+    
+    if (i%2 != 0){
+        console.log(i);
+    }
+}
---
+++ loopWithRange.js
@@ -0,0 +1,6 @@
+let awal = 4
+let akhir = 8
+
+for (let i = awal; i < akhir+1; i++){
+    console.log(i)
+}
